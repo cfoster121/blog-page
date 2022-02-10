21a03bc1aa47a0502055df7eaefea2ef1333d49c
Courtney Foster <cfoster649@gmail.com>
2022-02-09 19:37:39 -0800

    Models

---
+++ models/Blog.js
@@ -0,0 +1,13 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection')
+
+class Blog extends Model{}
+
+Blog.init({
+    title: DataTypes.STRING,
+    body: DataTypes.STRING,
+},
+{sequelize}
+)
+
+module.exports = Blog
