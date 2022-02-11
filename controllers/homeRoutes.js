125fd338966f202859c95c5b3571a1e6f77fcd0c
Courtney Foster <cfoster649@gmail.com>
2022-02-10 19:45:17 -0800

    Created home route to show all blog posts, user, and comments

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,13 +4,33 @@ const { User, Blog, Comment } = require('../models/');
 
 router.get("/", (req, res) => {
     Blog.findAll({
-})
+        attributes: ["title", "body"],
+        include: [
+            {
+                model: User,
+                attributes: ["username"],
+            },
+            {
+                model: Comment,
+                attributes: ["body"]
+            }
+        ],
+    })
         .then((dbBlogData) => {
             res.json(dbBlogData);
             if (!dbBlogData) {
                 res.status(404).json({ message: "No blogs" });
                 return;
             }
+
+            const blogs = blogData.map((blog) => blog.get({ plain: true }));
+
+
+            res.render('homepage', {
+                blogs,
+                logged_in: req.session.logged_in
+            });
+
         })
         .catch((err) => {
             console.log(err);
