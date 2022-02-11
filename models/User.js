08a3c3fa21c8ab814066391b7814a737f9fd76eb
Courtney Foster <cfoster649@gmail.com>
2022-02-10 17:50:19 -0800

    Set up user routes

--- models/User.js
+++ models/User.js
@@ -2,29 +2,29 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection')
 class User extends Model { }
 
-User.init ({
+User.init({
     id: {
         type: DataTypes.INTEGER,
-            allowNull: false,
-                primaryKey: true,
-                    autoIncrement: true
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
     },
     username: {
         type: DataTypes.STRING,
-            allowNull: false,
+        allowNull: false,
     },
     password: {
         type: DataTypes.STRING,
-            allowNull: false, 
+        allowNull: false,
     },
 },
-{
-    sequelize,
+    {
+        sequelize,
         timestamps: false,
-            freezeTableName: true,
-                underscored: true,
-                    modelName: 'user',
-      }
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+    }
 )
 
 module.exports = User;
